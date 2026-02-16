f5d1866ff373b8989d24807204c999d45f238dc7
gagan251 <142501382+gagan251@users.noreply.github.com>
2026-02-16 22:42:15 +0530

    Add files via upload

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5814286313-4a043",
+  "appId": "1:50657845773:web:87d2bacd3e183e473b2b44",
+  "apiKey": "AIzaSyClMOD0RWkHsV2Gr89MQAJrpeoYtGglvDA",
+  "authDomain": "studio-5814286313-4a043.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "50657845773"
+};
